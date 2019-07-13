e311046040f4d2c36619049ba6e602b2cc58112d
spider <85006@qq.com>
2019-07-13 13:48:57 +0800

    update

--- mysql57.sh
+++ mysql57.sh
@@ -39,6 +39,7 @@ inst(){
 }
 
 osver=`rpm -qi centos-release|egrep Version|awk '{print $3}'`
+cd /opt/mysql-rpms/
 if [[ "${osver}" == 6 ]] && [[ `arch` == x86_64 ]];then
     wget -c https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-community-client-5.7.26-1.el6.x86_64.rpm &
     wget -c https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-community-common-5.7.26-1.el6.x86_64.rpm &
